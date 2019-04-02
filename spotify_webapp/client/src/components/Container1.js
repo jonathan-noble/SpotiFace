4f70508af201f7d591fcb6467415dec228942212
jonathan-noble <c15487922@mydit.ie>
2019-04-02 06:45:08 +0100

    removed mock data

--- spotify_webapp/client/src/components/Container1.js
+++ spotify_webapp/client/src/components/Container1.js
@@ -45,44 +45,6 @@ export default class Container1 extends Component {
 
     scroll.scrollTo(600); 
 
-    let predictions = {
-      "angry":50,
-      "sad": 64,
-      "happy":52,
-      "neutral":10,
-      "surprised":23,
-      "scared": 5
-    }
-  
-    const keys = Object.keys(predictions);
-    const arrayOfPreds = keys.map(key => ({
-      mood: key, 
-      predict: predictions[key]
-      })
-    );
-  
-    console.log(arrayOfPreds);
-  
-    let highestPred = arrayOfPreds.reduce((prev, curr) => {
-      return prev.predict > curr.predict ? prev : curr;
-      });
-  
-    console.log(highestPred.id, highestPred.predict); 
-  
-  
-    let orderedPred = arrayOfPreds.sort((a, b) => {
-        return b.predict - a.predict;
-    });
-    
-    console.log(orderedPred[0]);
-    console.log(orderedPred[0].id);
-    
-    this.setState({
-      predictions: orderedPred
-    })
-  
-  
-
     this.predictData(message);
   }
       
@@ -264,6 +226,5 @@ export default class Container1 extends Component {
     </section>  
     );
   }
-
-    
+ 
 }
